e5f020257db0c062d42913afbc3dc07dc55f8233
Roberto Josafat Vallejo Reyes <133998389+KaiserAckerman@users.noreply.github.com>
2025-02-14 16:37:45 -0500

    CORRECION DE NOMBRE

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://RobertoVallejo.github.com',
+    site: 'https://KaiserAckerman.github.com',
     integrations: [
         mdx(),
         sitemap(),
