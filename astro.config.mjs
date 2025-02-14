3f0e3319266ad9facbf0dc1f41f837c75b64b066
Roberto Josafat Vallejo Reyes <133998389+KaiserAckerman@users.noreply.github.com>
2025-02-14 16:35:46 -0500

    Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://RobertoVallejo.github.io',
+    site: 'https://RobertoVallejo.github.com',
     integrations: [
         mdx(),
         sitemap(),
